bff1a12dff6d502ec3c083c636cc23f2a1c160a5
Daniel Herrera <dher426@wgu.edu>
2025-05-20 23:50:43 -0700

    working through state management

--- movie-frontend-app/src/App.jsx
+++ movie-frontend-app/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 
 function App() {
+
   return (
     <>
-      {/* parent element : one must be returned */}
-      <MovieCard movie={{title: "Daniel's Film", release_date: "2025"}}/>
+    <Home />
     </>
   );
 }
